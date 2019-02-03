594ecf20b57e460293f9fb6b85c56e7983dd1af8
Nick Florentius <donutpop365@gmail.com>
2019-02-03 10:58:18 -0600

    Update server.py

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import os
 
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from DataEvaluatorBuilder import DataEvalBuilder
+from DataEvalBuilder import DataEvalBuilder
 
 app = Flask(__name__)
 CORS(app)
