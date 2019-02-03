528754208114ec06af23f0cf38ed2cde871b3af2
Matthew Bundy <home@ebs-er-10-145-61-96.wireless.umn.edu>
2019-02-03 04:32:12 -0600

    added data

--- server.py
+++ server.py
@@ -9,4 +9,13 @@ CORS(app)
 def parse_inputs():
     zip = int(request.args.get('zip'))
     crop_list = request.args.get('crops').split(',')
-    pass
+    soybeanPriceData = "./data/soybean_price.csv"
+    builder = DataEvaluatorBuilder(zip,crop_list[0])
+    evaluator = builder.get("prices")
+    evaluator.evaluate()
+
+    json_as_str = ''
+    with open("./jsons/" +  str(zip) + str(cropt_list[0]) + ".json") as f:
+        json_as_str f.read()
+        
+    return json_as_str
