c1d3f9e6dd9d53969da0c71d9bce8a321beb81ea
ehiggi <ehiggi@blackhillsip.com>
2019-02-03 03:17:15 -0600

    Added Nav-Bar

---
+++ main.js
@@ -0,0 +1,96 @@
+Vue.component('landing-page', {
+    template: `
+    <div class="container">
+        <div class="row">
+            <div>
+                <div class="col-sm-12 col-md-10 col-md-offset-1">
+                <label>Enter Zip Code:</label>
+                <input type="text" v-model="zip">
+                <select class="mdb-select md-form" v-model="crops" multiple>
+                    <option>Corn</option>
+                    <option>Wheat</option>
+                    <option>Barley</option>
+                    <option>Alfalfa</option>
+                    <option>Cotton</option>
+                    <option>Cowmoji</option>
+                </select>
+                <button @click="lookup">FARM SEARCH</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    `,
+    data() {
+        return {
+            zip: 0,
+            crops: []
+        }
+    },
+    methods:{
+        lookup:function () {
+            this.$emit('complete-search',this.zip, this.crops)
+        }
+    }
+});
+Vue.component('tile', {
+    props: {
+
+    },
+    template: `
+    <div class="container">
+        <div class="col-xl">
+            <div>
+                <label>Enter Zip Code:</label>
+                <input type="text" v-model="zip">
+                <select class="mdb-select md-form" v-model="crops" multiple>
+                    <option>Corn</option>
+                    <option>Wheat</option>
+                    <option>Barley</option>
+                    <option>Alfalfa</option>
+                    <option>Cotton</option>
+                    <option>Cowmoji</option>
+                </select>
+                <button @click="lookup">FARM SEARCH</button>
+            </div>
+        </div>
+    </div>
+    `,
+    data() {
+        return {
+            zip: this.zipProp,
+            crops: this.cropsProp
+        }
+    },
+    methods:{
+        lookup:function () {
+            this.$emit('complete-search',this.zip, this.crops)
+        }
+    }
+});
+var app= new Vue({
+    el: '#app',
+    data:{
+        fullData: [],
+        zipCode: 0,
+        crops: [],
+        hasSearched: false
+    },
+    methods:{
+        search: function(zip,crops){
+            this.hasSearched = true;
+            this.zipCode = zip;
+            this.crops = crops;
+        }
+    }
+
+});
+
+
+function fetch_all(zip,crops) {
+    var cropstr = '';
+    for (var crop in crops) {
+        cropstr += crop;
+    }
+
+fetch(`http://${window.location.hostname}:5000/data?zip=${zip}&crops=${cropstr}`, {}) .then(response => response.json()) .then(success => (app.$data.fullData = success)) .catch(error => console.log(error))
+}
