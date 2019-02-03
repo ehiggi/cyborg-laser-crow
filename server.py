c6b21223a38e4f4cdbf4b0fe084cbd3cf1211015
ehiggi <ehiggi@blackhillsip.com>
2019-02-03 08:05:49 -0600

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from DataEvaluatorBuilder import DataEvalBuilder
 
 app = Flask(__name__)
 CORS(app)
@@ -10,12 +11,12 @@ def parse_inputs():
     zip = int(request.args.get('zip'))
     crop_list = request.args.get('crops').split(',')
     soybeanPriceData = "./data/soybean_price.csv"
-    builder = DataEvaluatorBuilder(zip,crop_list[0])
+    builder = DataEvalBuilder(zip,crop_list[0])
     evaluator = builder.get("prices")
     evaluator.evaluate()
 
     json_as_str = ''
-    with open("./jsons/" +  str(zip) + str(cropt_list[0]) + ".json") as f:
-        json_as_str f.read()
+    with open("./jsons/" + str(zip) + str(crop_list[0]) + ".json") as f:
+        json_as_str = f.read()
         
     return json_as_str
