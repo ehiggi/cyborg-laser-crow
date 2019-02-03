0ea0243b8ba15228eb923934f059ef9ea4c2f396
ehiggi <ehiggi@blackhillsip.com>
2019-02-03 10:58:34 -0600

    Create DataEvaluatorBuilder.py

---
+++ DataEvaluatorBuilder.py
@@ -0,0 +1,36 @@
+import sys
+sys.path.append("../evaluators")
+from PriceDataEvaluator import PriceDataEvaluator
+
+#dictates which data evaluator object to get
+class DataEvalBuilder (object):
+    def __init__(self,location,cropType):
+        self.zip = location
+        self.crop = cropType
+
+    def setLocation(self,location):
+        self.zip = location
+    def getLocation(self):
+        return self.zip
+    def setCropType(self,cropType):
+        self.crop = cropType
+    def getCropType(self):
+        return self.crop
+
+    def get(self,dataType):
+        dataType = dataType.lower()
+        if dataType == "memes":
+            return MemeEvaluator(self.zip,self.crop)
+        elif dataType == "weather":
+            return WeatherEvaluator(self.zip,self.crop)
+        elif dataType == "water":
+            return GroundWaterEvaluator(self.zip,self.crop)
+        elif dataType == "disease":
+            return DiseaseInfoEvaluator(self.zip,self.crop)
+        elif dataType == "prices":
+            #ok got this one done
+            return PriceDataEvaluator(self.zip,self.crop)
+        elif dataType == "news":
+            return NewsDataEvaluator(self.zip,self.crop)
+        else:
+            raise LookupError
